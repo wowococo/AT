0c2149da18501684e9553a5bb6c8f6904907f3b0
wowococo <wowococo@github.com>
2020-11-17 06:57:12 +0800

    add

--- test_tag/at_tag.py
+++ test_tag/at_tag.py
@@ -7,7 +7,7 @@ import pytest
 
 
 REQUEST_RESOURCE = ['tag', 'group', 'link']
-
+POST
 
 # class TestTag():
 
@@ -37,43 +37,60 @@ def extract_conf(resource):
         return conf['link']
 
 def send_post(resource):
-    resource = extract_conf(resource)['POST']
-    url = resource['url']
-    body = resource['body']
-    # keys, values = body['legal_keys'], body['values']
-    
-    for testcase in range(len(body.keys())):
-        data = json.dumps(testcase)
-        res = send_request('POST', url, data)
-        yield res
+    resource = extract_conf(resource).get('POST')
+    if resource:
+        url = resource['url']
+        body = resource['body']
+        for case in body.keys():
+            data = json.dumps(body[case])
+            res = send_request('POST', url, data)
+            yield res
+
+def send_put(resource):
+    resource = extract_conf(resource).get('PUT')
 
 def combine(resource, method):
-    res = send_post(resource)
-    expected_res = extract_conf(resource)[method]['expected_res']
+    # res = send_post(resource)
+    res = eval(f"send_{method.lower()}")
+    expected_res = extract_conf(resource).get(method).get('expected_res')
     return zip(res, expected_res)
 
 
 @pytest.mark.parametrize("a, expected", 
                          (lambda resource, method: combine(resource, method))('link', 'POST'))
 def test_send_post_link(a, expected):
-    assert a.status_code == expected[0]
-    text = json.loads(a.text)
-    if "code" in text:
-        assert text['code'] == expected[1]
-    if 'message' in text:
-        assert text['message'] == expected[2]
+    start_assert(a, expected)
+    # assert a.status_code == expected[0]
+    # text = json.loads(a.text)
+    # if "code" in text:
+    #     assert text['code'] == expected[1]
+    # if 'message' in text:
+    #     assert text['message'] == expected[2]
 
 
 @pytest.mark.parametrize("a, expected", 
                          (lambda resource, method: combine(resource, method))('tag', 'POST'))
 def test_send_post_tag(a, expected):
+    start_assert(a, expected)
+    # assert a.status_code == expected[0]
+    # text = json.loads(a.text)
+    # if "code" in text:
+    #     assert text['code'] == expected[1]
+    # if 'message' in text:
+    #     assert text['message'] == expected[2]
+
+@pytest.mark.parametrize("a, expected", 
+                         (lambda resource, method: combine(resource, method))('tag', 'PUT'))
+def test_put_post_tag(a, expected):
+    start_assert(a, expected)
+
+
+def start_assert(a, expected):
     assert a.status_code == expected[0]
     text = json.loads(a.text)
     if "code" in text:
         assert text['code'] == expected[1]
     if 'message' in text:
         assert text['message'] == expected[2]
-    
-
 
 
